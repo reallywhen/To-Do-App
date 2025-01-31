55fab3e21f81f62e9d90c2672451ada0616ff58f
brent <arandomperson372@gmail.com>
2025-01-30 18:33:02 -0800

    removed 'null' and fixed double input

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,27 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static String[] toDoStorage = new String[100];
+    static Scanner toDoInput = new Scanner(System.in);
+    static int nullStopper = 0;
     public static void main(String[] args) {
-        toDoLister();
+       for(var i = 0; i < i + 1; i++) {
+           toDoLister();
+       }
 
 
     }
 
     public static void toDoLister () {
-        String[] toDoStorage = new String[3];
-        Scanner toDoInput = new Scanner(System.in);
-        int nullStopper = 0;
+
 
         System.out.println("Please enter a thing you have to-do");
 
-        for (var i = 0; i < toDoStorage.length; i++){
-            nullStopper = nullStopper + 1;
-            System.out.println("looped");
+        for (var i = nullStopper; i < toDoStorage.length;i = nullStopper){
+
+
             toDoStorage[i] = toDoInput.nextLine();
+            nullStopper++;
             System.out.println("Would you like to enter any more tasks? Enter \"yes\" or \"no\" if not.");
             if (toDoInput.nextLine().equals("no")){
                 System.out.println("Your task(s) are to " + toDoStorage[i] + ".");
@@ -30,6 +34,7 @@ public class Main {
 
 
         System.out.println("All tasks below");
+
         for (var i = 0; i < nullStopper; i++) {
             System.out.println("-"+toDoStorage[i]);
         }
@@ -38,20 +43,5 @@ public class Main {
 
     }
 
-   public static void reference (){
-    System.out.println("Please enter a thing you have to-do");
-    Scanner toDoInput = new Scanner(System.in);
-    String task1 = toDoInput.nextLine();
-        System.out.println("Would you like to enter any more tasks? Enter \"no\" if not.");
-    String task2 = toDoInput.nextLine();
-        if (task2.equals("no")) {
-        System.out.println("Your singular task is to " + task1 + ".");
 
-
-
-    }
-        else {
-        System.out.println("Your tasks are; " + task1 + ", " + task2 + ".");
-    }
-}
 }
