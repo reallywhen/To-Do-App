a17470eba08d7b9b8c1271583a641202e40ecc95
brent <arandomperson372@gmail.com>
2025-02-06 18:39:55 -0800

    removed 'null' and fixed double input

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
-
+// thinl about how            to delete AFTER making sure it works in ALL circumstances
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static String[] toDoStorage = new String[100];
     static Scanner toDoInput = new Scanner(System.in);
     static int nullStopper = 0;
+    static String NextStep = "x";
     public static void main(String[] args) {
-       for(var i = 0; i < i + 1; i++) {
-           toDoLister();
-       }
+        for(var i = 0; i < i + 1; i++) {
+            System.out.println("Would you like to Add, View, or Delete?");
+            NextStep = toDoInput.nextLine();
+            if (NextStep.equals("add")) {
+                toDoLister();
+            }
+            if (NextStep.equals("view")) {
+                System.out.println("All tasks below");
 
-
+                for (var j = 0; j < nullStopper; j++) {
+                    System.out.println("-" + toDoStorage[j]);
+                }
+            }
+        }
     }
 
     public static void toDoLister () {
@@ -26,7 +36,6 @@ public class Main {
             nullStopper++;
             System.out.println("Would you like to enter any more tasks? Enter \"yes\" or \"no\" if not.");
             if (toDoInput.nextLine().equals("no")){
-                System.out.println("Your task(s) are to " + toDoStorage[i] + ".");
                 break;
             }
 
